ac8474c8fecc57738dad9122cd352cf992a3f9d5
Mark <mindmark@gmail.com>
2013-04-27 14:23:54 -0700

    updated setup info

--- setup.py
+++ setup.py
@@ -19,9 +19,27 @@ else:
     extension_kwargs['extra_link_args'] = shlex.split(libs)
 
 setup(
+    name="cythonmagick",
+    version="0.1.1",
+    description="Python Bindings for ImageMagick Magick++ API written in Cython",
+    url='https://github.com/markreidvfx/cythonmagick',
+    
+    author='Mark Reid',
+    author_email='mindmark@gmail.com',
+    license='Apache License (2.0)',
+    
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension('cythonmagick',["cythonmagick/core.pyx"],
                                              language="c++",
-                                             **extension_kwargs)]
+                                             **extension_kwargs)],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Software Development :: Libraries',
+    ],
 )
 
