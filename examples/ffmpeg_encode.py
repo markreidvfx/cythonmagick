4fdb95372074a549feef2f7bd326d034fab410ef
Mark <mindmark@gmail.com>
2013-04-29 12:51:39 -0700

    removed unused import

--- examples/ffmpeg_encode.py
+++ examples/ffmpeg_encode.py
@@ -1,6 +1,5 @@
 
 import time
-from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor
 
 from multiprocessing.dummy import Pool #Pool.imap handles memory better then ThreadPoolExecutor.map
 
