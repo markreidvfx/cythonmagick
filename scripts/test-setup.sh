5d439f1ecd87486666f1c70e5c20241a3deee0d8
Mark Reid <mindmark@gmail.com>
2015-01-16 20:37:50 -0800

    updated travis test

--- scripts/test-setup.sh
+++ scripts/test-setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [[ $MAGICKVERSION == latest ]]; then
+if [[ $MAGICKVERSION == current ]]; then
+
+	sudo apt-get update -qq
+	sudo apt-get install libmagick++-dev --fix-missing
+
+else
 
 	sudo apt-get update -qq
 	sudo apt-get build-dep imagemagick --fix-missing
 	sudo apt-get 
-	wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+	wget http://www.imagemagick.org/download/releases/ImageMagick-$MAGICKVERSION.tar.gz
 	mkdir ImageMagick
-	tar -xf ImageMagick.tar.gz -C ImageMagick --strip-components=1
+	tar -xf ImageMagick-$MAGICKVERSION.tar.gz -C ImageMagick --strip-components=1
 	cd ImageMagick
 	./configure --enable-hdri --without-lcms2 --without-lcms 
 	make
 	sudo make install
 	cd ..
-
-
-else
-	sudo apt-get update -qq
-	sudo apt-get install libmagick++-dev --fix-missing
 
 fi
 
